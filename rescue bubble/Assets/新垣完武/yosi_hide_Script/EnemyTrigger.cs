eb1ead66ccce88827eea4f119f5015c73b8f6d79
yoshihidefunakoshi <11807114@stu.kbc.ac.jp>
2020-04-03 13:01:21 +0900

    EnemyTriggerの更新

---
+++ rescue bubble/Assets/新垣完武/yosi_hide_Script/EnemyTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTrigger : MonoBehaviour {
+
+
+    public GameObject Enemy;
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(other.gameObject.CompareTag("Player")) {
+            Enemy.SetActive(true);
+            Enemy1.SetActive(true);
+            Enemy2.SetActive(true);
+            GetComponent<BoxCollider>().enabled = false;
+
+        }
+    }
+}
