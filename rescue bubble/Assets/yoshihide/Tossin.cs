dbde1ee07ee4017f65b42b29c516473b715d227d
yoshihidefunakoshi <11807114@stu.kbc.ac.jp>
2020-04-09 23:00:01 +0900

    突進型エネミーの更新です。

---
+++ rescue bubble/Assets/yoshihide/Tossin.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tossin : MonoBehaviour
+{
+
+    //Vector3 tmpE;                        //プレイヤーの位置情報をここに入れる 
+    //Vector3 t;
+    //public Transform hogeEne;
+    //int Ecnt = 0;                       //フラグ立てに使う
+    //float calcZ = 0;                    //敵とプレイヤーの距離を測るのに使う
+    GameObject Player;
+    private float targetTime = 2.0f;
+    private float currentTime = 0;
+    
+    public GameObject Enemy;
+    float speed = 2.0f;
+    private Vector3 vec;
+
+    void Start()
+    {
+         Player = GameObject.Find("Player");
+        
+    //    Vector3 playerpos = this.Player.transform.position;
+    //    Rigidbody rb = GetComponent<Rigidbody>();
+    //    rb.collisionDetectionMode = CollisionDetectionMode.Discrete;
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (targetTime < currentTime)
+        {
+            currentTime = 0;
+            //敵の座標を変数posに保存
+            var pos = this.gameObject.transform.position;
+
+            //敵の位置をプレイヤーの位置にする
+            //this.transform.position = pos;
+
+            //敵からプレイヤーに向かうベクトルをつくる
+            //プレイヤーの位置から敵の位置（弾の位置）を引く
+            //this.calcZ = pos.z - Player.transform.position;
+            Vector3 vec = Player.transform.position - pos;
+
+            //弾のRigidBodyコンポネントのvelocityに先程求めたベクトルを入れて力を加える
+            gameObject.GetComponent<Rigidbody>().velocity = vec;
+            //float step = speed * Time.deltaTime;
+            //transform.position = Vector3.MoveTowards(pos, Player.transform.position, 0.8f);
+
+
+            //Vector3 playerpos = this.Player.transform.position;                 //player位置情報取得する
+            //this.calcZ = this.transform.position.z - playerpos.z;               //敵のZ座標からボールのZ座標を引く。これで距離が出る
+
+            //tmpE = hogeEne.transform.position;                                  //プレイヤーの位置情報をvector3でtmpに格納
+            //this.transform.position = t;
+        }
+
+
+        //Vector3 playerpos = this.Player.transform.position;
+        //InvokeRepeating("Attack", 2, 4);
+        //Invoke("Cancel", 5);
+    }
+}
+    //void OncollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.name == "Player")
+    //    {
+    //        Debug.Log("ssss");
+    //        hogeEne = GameObject.Find("Player").GetComponent<Transform>();
+    //        Ecnt = 0;
+
+    //    }
+    //}
+
+//    void Attack()
+//    {
+//        Vector3 playerpos = this.Player.transform.position;                 //player位置情報取得する
+//        this.calcZ = this.transform.position.z - playerpos.z;               //敵のZ座標からボールのZ座標を引く。これで距離が出る
+//        if(this.calcZ <50)
+//        {
+//            if(this.Ecnt == 0)
+//            {
+//                hogeEne = GameObject.Find("Player").GetComponent<Transform>();      //プレイヤーを見つける
+//                tmpE = hogeEne.transform.position;                                  //プレイヤーの位置情報をvector3でtmpに格納
+//                this.Ecnt = 2;
+//            }
+//            if (this.Ecnt == 2)
+//            {
+//                
+//                Debug.Log("aa");
+//                //this.transform.position = Vector3.MoveTowards(this.transform.position, new Vector3(tmpE.x, tmpE.y, tmpE.z), 0.2f);
+//            }
+//        }
+//    }       
+//}
+
+
