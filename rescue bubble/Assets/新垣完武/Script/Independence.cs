01f8498742e7d681da3d5b801f30034f11412785
Tepodon <hiromu13411234@gmail.com>
2020-04-23 15:30:19 +0900

    プレイヤーのショットにエフェクトを追加。それにともないスクリプトとプレハブを追加。

---
+++ rescue bubble/Assets/新垣完武/Script/Independence.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Independence : MonoBehaviour {
+    public float timer;
+	// Use this for initialization
+	void Start () {
+        gameObject.transform.parent = null; //親離れをする
+        timer = timer * 60; //普通の秒数に直す
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer -= 1;
+        if (timer <= 0)
+        {
+            timer = 0;
+            Destroy(gameObject, timer);
+        }
+
+	}
+}
