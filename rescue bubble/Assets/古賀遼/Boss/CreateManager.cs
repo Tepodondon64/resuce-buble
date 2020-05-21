a39f9a7eb6e633abe1aa3be986c26636cb5b955c
Haruka29560 <11806006@stu.kbc.ac.jp>
2020-05-21 18:36:07 +0900

    ボスの動き追加

--- rescue bubble/Assets/古賀遼/Boss/CreateManager.cs
+++ rescue bubble/Assets/古賀遼/Boss/CreateManager.cs
@@ -12,40 +12,56 @@ public class CreateManager : MonoBehaviour
 
     int eCount;
 
+    private int P = 0;
+    public int S = 0;
+    public int K = 0;
+
     public EnemyStatus enemystatus;
 
     //public float speed = 1000;
     // 初期化
     void Start()
     {
-        //ボスのHPをいれる変数
         
-        //EnemyStatusの数値を取得
-        eCount = enemystatus.enemy_hp;
-        Debug.Log(eCount); //5
         target = GameObject.Find("Player").transform;
-        //Vector3 MSPO = GameObject.Find("MS").transform.position;
         StartCoroutine ("ChangeColor1");
+    }
+
+    void Update()
+    {
+        //ボスのHPを毎回取る
+        eCount = enemystatus.enemy_hp;
+
     }
 
     IEnumerator ChangeColor1()
     {
         yield return new WaitForSeconds(0);
 
-        //// プレハブを元にオブジェクトを生成する 火炎放射
-        //GameObject obj = (GameObject)Resources.Load("Flameradiation");
-        //GameObject instance = (GameObject)Instantiate(obj,new Vector3(-6.36f, 9.01f, -4f), Quaternion.identity);
-
         // プレハブを元にオブジェクトを生成する
-        Invoke("Call", 5f);
+        Invoke("Call", 2f);
+        Invoke("kaenud", 1f);
+        
         Debug.Log("これはできてる");
+        Debug.Log(eCount); //5
 
         //5秒停止
         yield return new WaitForSeconds(10);
-
         Debug.Log(eCount); //5
-        //もう一つのコルーチンを実行する
-        StartCoroutine("ChangeColor2");
+
+        if (eCount < 500)
+        {
+            if(P == 0)
+            {
+                StartCoroutine("ChangeColor4");
+                P++;
+            }
+
+        }else{
+            //もう一つのコルーチンを実行する
+            //StartCoroutine("ChangeColor2");
+            StartCoroutine("ChangeColor3");
+        }
     }
 
     IEnumerator ChangeColor2()
@@ -58,7 +74,7 @@ public class CreateManager : MonoBehaviour
         yield return new WaitForSeconds(10);
 
         //もう一つのコルーチンを実行する
-        StartCoroutine("ChangeColor1");
+        StartCoroutine("ChangeColor3");
         Debug.Log(eCount); //5
     }
 
@@ -66,14 +82,24 @@ public class CreateManager : MonoBehaviour
     {
 
         // プレハブを元にオブジェクトを生成する
-        Invoke("Call", 1f);
+        Invoke("yobi", 5f);
 
-        //10秒停止
-        yield return new WaitForSeconds(4);
+        //5秒停止
+        yield return new WaitForSeconds(15);
 
         //もう一つのコルーチンを実行する
         StartCoroutine("ChangeColor1");
+        Debug.Log(eCount); //5
+    }
 
+    IEnumerator ChangeColor4()
+    {
+        Debug.Log("よくここまで来たね");
+
+        Invoke("SENP", 0);
+        Invoke("mark", 0);
+
+        yield return new WaitForSeconds(10);
     }
 
     //void Update()
@@ -142,36 +168,211 @@ public class CreateManager : MonoBehaviour
         GameObject obj = (GameObject)Resources.Load("SFB");
         GameObject instance = (GameObject)Instantiate(obj,tmp, Quaternion.identity);
     }
-    //void FB2()
-    //{
-    //    GameObject obj = (GameObject)Resources.Load("SFB");
-    //    GameObject instance = (GameObject)Instantiate(obj, new Vector3(10.0f, 30.0f, -11.3f), Quaternion.identity);
-    //}
 
-    //void FB3()
-    //{
-    //    GameObject obj = (GameObject)Resources.Load("SFB");
-    //    GameObject instance = (GameObject)Instantiate(obj, new Vector3(0.0f, 30.0f, -11.3f), Quaternion.identity);
-    //}
-
-    //void FB4()
-    //{
-    //    GameObject obj = (GameObject)Resources.Load("SFB");
-    //    GameObject instance = (GameObject)Instantiate(obj, new Vector3(-10.0f, 30.0f, -11.3f), Quaternion.identity);
-    //}
-
-    //void FB5()
-    //{
-    //    GameObject obj = (GameObject)Resources.Load("SFB");
-    //    GameObject instance = (GameObject)Instantiate(obj, new Vector3(-20.0f, 30.0f, -11.3f), Quaternion.identity);
-    //    //witch = false;
-    //}
-
-    void FD()
+    void SENP()
     {
-        Destroy(this.gameObject);
+        //火災旋風の呼び出し
+        GameObject obj9 = (GameObject)Resources.Load("senpu");
+        GameObject instance9 = (GameObject)Instantiate(obj9, new Vector3(0, -0.5f, 0), Quaternion.identity);
+
+        Destroy(instance9, 10f);
+        Invoke("mark", 5);
+    }
+
+    void mark()
+    {
+        //予測呼び出し（四角）
+        GameObject yotyou1 = (GameObject)Resources.Load("yosokusikaku");
+        GameObject yoyo1 = (GameObject)Instantiate(yotyou1, new Vector3(0, 1f, 0), Quaternion.identity);
+
+        //予測呼び出し（丸）
+        //GameObject yotyou2 = (GameObject)Resources.Load("yosokumaru");
+        //GameObject yoyo2 = (GameObject)Instantiate(yotyou2, new Vector3(0, 1f, 0), Quaternion.identity);
+
+        //Destroy(yoyo1,5f);
+        Destroy(yoyo1, 5f);
+    }
+
+    void kaenud()
+    {
+        //右肩
+        Transform RTATransform1 = GameObject.Find("Rightshoulder").transform;
+
+        Vector3 Rotation33 = GameObject.Find("Rightshoulder").transform.localEulerAngles;
+        Rotation33.x = 90.0f;
+        Rotation33.y = 0.0f;
+        Rotation33.z = 0.0f;
+        S++;
+
+        RTATransform1.localEulerAngles = Rotation33;
+
+        Invoke("kaend", 10);
+    }
+
+    void kaend()
+    {
+        Transform RTATransform1 = GameObject.Find("Rightshoulder").transform;
+
+        Vector3 Rotation33 = GameObject.Find("Rightshoulder").transform.localEulerAngles;
+        Rotation33.x = 0.0f;
+        Rotation33.y = 0.0f;
+        Rotation33.z = 0.0f;
+
+        RTATransform1.localEulerAngles = Rotation33;
 
     }
+    void yobi()
+    {
+        //if (S < 18)
+        //{
+        //    transform.rotation = transform.rotation * Quaternion.Euler(5, 0, 0);
+        //    Debug.Log(gameObject.transform.localEulerAngles);
+        //    S++;
+        //    //K = 0;            
+        //}
+
+        //if()
+
+        //右肩
+        Transform RTATransform1 = GameObject.Find("Rightshoulder").transform;
+
+        Vector3 Rotation33 = GameObject.Find("Rightshoulder").transform.localEulerAngles;
+        //if (Rotation33.x <90.0f)
+        //if(S<18)
+        //
+            Rotation33.x = 75.0f;
+            Rotation33.y = 50.0f;
+            Rotation33.z = 0.0f;
+            
+        //}
+
+        RTATransform1.localEulerAngles = Rotation33;
+
+        //右腕
+        Transform RTATransform2 = GameObject.Find("Rightarm").transform;
+        Vector3 Rotation44 = GameObject.Find("Rightarm").transform.localEulerAngles;
+        //if (Rotation33.x <90.0f)
+        //if (S < 18)
+        //{
+            Rotation44.x = 0.0f;
+            Rotation44.y = 0.0f;
+            Rotation44.z = 50.0f;
+            
+        //}
+
+        RTATransform2.localEulerAngles = Rotation44;
+
+        //体をひねる
+        Transform RTATransform3 = GameObject.Find("Body").transform;
+        Vector3 Rotation55 = GameObject.Find("Body").transform.localEulerAngles;
+        //if (Rotation33.x <90.0f)
+        //if (S < 18)
+        //{
+        Rotation55.x = 0.0f;
+        Rotation55.y = 12.0f;
+        Rotation55.z = 0.0f;
+        
+        //}
+
+        RTATransform3.localEulerAngles = Rotation55;
+
+        Invoke("PaPa", 5);
+    }
+
+    void PaPa()
+    {
+        //右肩
+        Transform RTATransform1 = GameObject.Find("Rightshoulder").transform;
+        Vector3 trans1 = RTATransform1.position;
+        Vector3 Rotation33 = GameObject.Find("Rightshoulder").transform.localEulerAngles;
+        //if (Rotation33.x <90.0f)
+        //if(S<18)
+        //trans1.x += 0.0f;
+        trans1.y = 9.0f;
+        trans1.z = -2.0f;
+
+        RTATransform1.localPosition = trans1;
+
+        Rotation33.x = 40.0f;
+        Rotation33.y = 0.0f;
+        Rotation33.z = 0.0f;
+        S++;
+    
+        RTATransform1.localEulerAngles = Rotation33;
+
+        //右腕
+        Transform RTATransform2 = GameObject.Find("Rightarm").transform;
+        Vector3 Rotation44 = GameObject.Find("Rightarm").transform.localEulerAngles;
+        //if (Rotation33.x <90.0f)
+        //if (S < 18)
+        //{
+        Rotation44.x = 0.0f;
+        Rotation44.y = 0.0f;
+        Rotation44.z = 0.0f;
+        S++;
+        //}
+
+        RTATransform2.localEulerAngles = Rotation44;
+
+        Invoke("modos", 5);
+
+        //体をひねる
+        Transform RTATransform3 = GameObject.Find("Body").transform;
+        Vector3 Rotation55 = GameObject.Find("Body").transform.localEulerAngles;
+        //if (Rotation33.x <90.0f)
+        //if (S < 18)
+        //{
+        Rotation55.x = 0.0f;
+        Rotation55.y = -12.0f;
+        Rotation55.z = 0.0f;
+
+        RTATransform3.localEulerAngles = Rotation55;
+    }
+
+    void modos()
+    {
+        //右肩
+        Transform RTATransform1 = GameObject.Find("Rightshoulder").transform;
+        Vector3 trans1 = RTATransform1.position;
+        Vector3 Rotation33 = GameObject.Find("Rightshoulder").transform.localEulerAngles;
+        //if (Rotation33.x <90.0f)
+        //if(S<18)
+        trans1.x = -6.443245f;
+        trans1.y = 12.20356f;
+        trans1.z = 0.5219421f;
+
+        RTATransform1.localPosition = trans1;
+
+        Rotation33.x = 0.0f;
+        Rotation33.y = 0.0f;
+        Rotation33.z = 0.0f;
+        S++;
+        //}
+
+        RTATransform1.localEulerAngles = Rotation33;
+
+        //体をひねる
+        Transform RTATransform3 = GameObject.Find("Body").transform;
+        Vector3 Rotation55 = GameObject.Find("Body").transform.localEulerAngles;
+        //if (Rotation33.x <90.0f)
+        //if (S < 18)
+        //{
+        Rotation55.x = 0.0f;
+        Rotation55.y = 0.0f;
+        Rotation55.z = 0.0f;
+        S++;
+        //}
+
+        RTATransform3.localEulerAngles = Rotation55;
+    }
+
+
+
+    //void FD()
+    //{
+    //    Destroy(this.gameObject);
+
+    //}
 
     //void Call2()
     //{
@@ -187,20 +388,5 @@ public class CreateManager : MonoBehaviour
     //    GameObject instance = (GameObject)Instantiate(obj, new Vector3(0.0f, 5.0f, 10.0f), Quaternion.identity);
 
     //}
-
-    void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.tag == "Untagged")
-        {
-            Debug.Log("Hit"); // ログを表示する
-            BOSSHP--;
-        }
-
-        if (BOSSHP == 0)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
 
 }
