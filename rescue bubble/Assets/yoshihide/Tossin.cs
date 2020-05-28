2037a904a839dbd22c1170801923993ec5a6c707
yoshihidefunakoshi <11807114@stu.kbc.ac.jp>
2020-05-28 17:29:36 +0900

    enemystatusとtossinと突進敵のプレハブの修正

--- rescue bubble/Assets/yoshihide/Tossin.cs
+++ rescue bubble/Assets/yoshihide/Tossin.cs
@@ -11,7 +11,7 @@ public class Tossin : MonoBehaviour
     //int Ecnt = 0;                       //フラグ立てに使う
     //float calcZ = 0;                    //敵とプレイヤーの距離を測るのに使う
     GameObject Player;
-    private float targetTime = 6.0f;
+    private float targetTime = 2.0f;
     private float currentTime = 0;
     //public Material material;
     public Transform _target;
@@ -24,9 +24,21 @@ public class Tossin : MonoBehaviour
     private Vector3 pos;
     private Rigidbody rb;
 
+    Transform myTransform;
+
     //効果音の設定
     AudioSource audioSource;                //オーディオを取得するための変数
     //public AudioClip TossinSE;              //突進時に再生させるための変数
+
+    //エフェクト
+    public Rush m_rushprefab;     //突進エフェクトのプレハブ
+
+    [SerializeField]
+    private float offsetx;
+    [SerializeField]
+    private float offsety;
+    [SerializeField]
+    private float offsetz;
     
     void Start()
     {
@@ -40,46 +52,29 @@ public class Tossin : MonoBehaviour
 
     void Update()
     {
-        
+        //transformを取得
+        myTransform = this.transform;
+
+        //座標を取得
+        //Vector3 pos = myTransform.position;
+
         currentTime += Time.deltaTime;
         if (targetTime < currentTime)
         {
             animator.SetBool("tossin", true);
 
-            //敵（イノシシ）の座標を変数posに保存
-            pos = this.gameObject.transform.position;
-
-            //プレイヤーの方を向く
-            Vector3 target = _target.position;
-            target.y = this.transform.position.y;
-            this.transform.LookAt(target);
-
-            AM.enabled = false;
-            //Vector3 force = new Vector3(0.0f, target.y, target.z-2.0f);
-
-            //現在の速度をログに表示
-            //Debug.Log(rb.velocity.magnitude);
-
-            //this.GetComponent<Renderer>().material.color = Color.white;
-
-
-            //currentTime = 0;
-
-            //敵からプレイヤーに向かうベクトルをつくる
-            //プレイヤーの位置から敵の位置（弾の位置）を引く
-            //this.calcZ = pos.z - Player.transform.position;
-            Vector3 vec = Player.transform.position - pos;
+            
 
             //弾のRigidBodyコンポネントのvelocityに先程求めたベクトルを入れて力を加える
-            gameObject.GetComponent<Rigidbody>().velocity = vec;
-            //GetComponent<Rigidbody>().AddForce(Player.transform.position * speed, ForceMode.Force);
+            //gameObject.GetComponent<Rigidbody>().velocity = vec;
+            
 
             //突進時に音を再生する
             //audioSource.PlayOneShot(TossinSE);
 
             //float step = speed * Time.deltaTime;
 
-            //Invoke("Attack", 1.0f);
+            Invoke("Attack",0.0f);
 
             //Vector3 playerpos = this.Player.transform.position;                 //player位置情報取得する
             //this.calcZ = this.transform.position.z - playerpos.z;               //敵のZ座標からボールのZ座標を引く。これで距離が出る
@@ -87,10 +82,50 @@ public class Tossin : MonoBehaviour
             //tmpE = hogeEne.transform.position;                                  //プレイヤーの位置情報をvector3でtmpに格納
             //this.transform.position = t;
 
+            
+        }
+
+        
+    }
+
+    
+    void OnCollisionEnter(Collision collision)
+    {
+        //プレイヤーと衝突した場合
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("ああ");
+            //エフェクトを生成する
+            Instantiate(m_rushprefab, this.transform.position + new Vector3(offsetx,offsety,offsetz), Quaternion.identity);
         }
     }
     void Attack()
     {
         //transform.position = Vector3.MoveTowards(pos, Player.transform.position, speed);
+        //敵（イノシシ）の座標を変数posに保存
+        pos = this.gameObject.transform.position;
+
+        //プレイヤーの方を向く
+        Vector3 target = _target.position;
+        target.y = this.transform.position.y;
+        this.transform.LookAt(target);
+
+        AM.enabled = false;
+        //Vector3 force = new Vector3(0.0f, target.y, target.z-2.0f);
+
+        //現在の速度をログに表示
+        //Debug.Log(rb.velocity.magnitude);
+
+        //this.GetComponent<Renderer>().material.color = Color.white;
+
+
+        //currentTime = 0;
+
+        //敵からプレイヤーに向かうベクトルをつくる
+        //プレイヤーの位置から敵の位置（弾の位置）を引く
+        //this.calcZ = pos.z - Player.transform.position;
+        Vector3 vec = Player.transform.position - pos;
+        gameObject.GetComponent<Rigidbody>().velocity = vec;
+        //GetComponent<Rigidbody>().AddForce(Player.transform.position * speed, ForceMode.Force);
     }
 }
