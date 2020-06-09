d0b03d2eb759f681adb914d0d5496b9a61a82ec7
Tepodon <hiromu13411234@gmail.com>
2020-06-09 14:49:56 +0900

    GameOverのシーン(全４つ)を更新。GameOverUIフォルダーの追加。GameOverのBGM、SEとボタン設定に必要なスクリプトを追加。

---
+++ rescue bubble/Assets/新垣完武/Script/GameOver_Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UIコンポーネントの使用
+
+public class GameOver_Menu : MonoBehaviour {
+
+    Button Hajimari;
+
+    Button End;
+
+    // Use this for initialization
+    void Start()
+    {
+        Hajimari = GameObject.Find("/GameOverWindow/Continue_Button").GetComponent<Button>();
+
+        End = GameObject.Find("/GameOverWindow/Title_Button").GetComponent<Button>();
+
+        // 最初に選択状態にしたいボタンの設定
+        Hajimari.Select();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
